03263970b7cdb73a5f283d1ccf301d857f6c013e
Mikko Rantanen <jubjub@jubjubnest.net>
2020-05-22 16:34:32 +0300

    Write a _very_ comprehensive test for oneof fields

---
+++ tests/oneof.rs
@@ -0,0 +1,23 @@
+#[test]
+fn oneof()
+{
+    use protofish::Context;
+
+    // Hey at least we're ensuring this doesn't panic. :<
+    Context::parse(&[r#"
+      syntax = "proto3";
+      message Message {
+          oneof a {
+            string a1 = 1;
+            string a2 = 2;
+            string a3 = 3;
+          };
+          oneof b {
+            uint32 b1 = 4;
+            uint32 b2 = 5;
+            uint32 b3 = 6;
+          }
+      }
+    "#])
+    .unwrap();
+}
